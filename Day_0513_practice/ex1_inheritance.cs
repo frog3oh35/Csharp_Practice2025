7881306e9a013090d88103057d3aba8c396a2ebd
Yeonha Park <frog3oh35@gmail.com>
2025-05-13 22:44:05 +0900

    study : Add inheritance & casting examples with summaryy (0513)

---
+++ Day_0513_practice/ex1_inheritance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+/* 
+class Animal
+{
+    public int Age;
+    public void Eat()
+    {
+        Console.WriteLine("냐미!");
+    }
+}
+
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("멍멍");
+    }
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Animal dog = new Dog();
+
+        dog.Eat();
+    
+        if (dog is Dog)
+        {
+            ((Dog)dog).Bark();
+        }
+    }
+}
+*/
---
+++ Day_0513_practice/ex2_casting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_0513_practice
+{
+    class Animal
+    {
+        public string Name;
+    }
+
+    class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("냐~");
+        }
+    }
+    internal class ex2_casting
+    {
+        static void Main()
+        {
+            Animal animal = new Cat();
+            Cat cat = animal as Cat;
+
+            if (cat != null)
+            {
+                cat.Meow();
+            }
+            else
+            {
+                Console.WriteLine("형변환 실패");
+            }
+        }
+    }
+}
